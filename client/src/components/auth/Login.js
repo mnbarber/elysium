e84e9da27e11e9046098b90fd686f7a753339b69
megan <mnbarber1993@gmail.com>
2026-01-23 12:34:50 -0800

    oops adding axios

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../../context/authContext';
+import axios from 'axios';
 import './Auth.css';
 
 function Login() {
